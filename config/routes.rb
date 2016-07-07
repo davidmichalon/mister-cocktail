4d4115f3b0e0dba77be4cb6183ada1f5ba1abe2a
Thomas Sertorio <thomas.sertorio@gmail.com>
2016-07-07 19:05:16 +0200

    Mister Cocktail

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  resources :cocktails, only: [:show, :index, :new, :create]
+  resources :cocktails, only: [:show, :index, :new, :create] do
+    resources :doses, only: [:create, :destroy]
+  end
+
   root "cocktails#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
